8da5bd6e6ac3a866f49897563ad9d97603f16002
Lord Turmoil <tony-turmoil@outlook.com>
2023-12-13 15:32:47 +0800

    fix: concept name

--- Beyond.SearchEngine/Modules/Search/Models/Concept.cs
+++ Beyond.SearchEngine/Modules/Search/Models/Concept.cs
@@ -9,15 +9,15 @@ public class Concept : OpenAlexStatisticsModel
     ///     Wikidata ID without prefix. e.g. Q21198
     ///     of https://www.wikidata.org/wiki/Q21198
     /// </summary>
-    [Column(TypeName = "varchar(8)")]
+    [Column(TypeName = "varchar(15)")]
     public string WikiDataId { get; set; }
 
     [NotMapped]
     public string FullWikiDataId => "https://www.wikidata.org/wiki/" + WikiDataId;
 
+    
     /***             Basics               ***/
 
-    [Column(TypeName = "varchar(63)")]
     public string Name { get; set; }
 
     public int Level { get; set; }
