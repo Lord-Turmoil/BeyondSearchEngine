b961c300d26837bd357d65be6d8f1ae5cabbc3d2
Lord Turmoil <tony-turmoil@outlook.com>
2023-12-18 22:27:22 +0800

    trivial

--- Beyond.SearchEngine/Modules/Globals.cs
+++ Beyond.SearchEngine/Modules/Globals.cs
@@ -3,12 +3,12 @@
 public static class Globals
 {
     public static readonly HashSet<string> AvailableTypes = [
-        "institutions",
         "authors",
-        "works",
         "concepts",
-        "sources",
+        "funders",
+        "institutions",
         "publishers",
-        "funders"
+        "sources",
+        "works",
     ];
 }
